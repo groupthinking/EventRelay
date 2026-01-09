62445817d165fce3ab7394a4e7ef398791327489
Hayden <154503486+groupthinking@users.noreply.github.com>
2026-01-09 02:47:35 -0600

    clean

--- _archive/netmesh-extension/background.js
+++
@@ -1,8 +0,0 @@
-// Enable side panel behavior
-chrome.sidePanel
-    .setPanelBehavior({ openPanelOnActionClick: true })
-    .catch((error) => console.error(error));
-
-chrome.runtime.onInstalled.addListener(() => {
-    console.log("NetMesh Vision installed");
-});
